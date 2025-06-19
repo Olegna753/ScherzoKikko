0d8d1959a135ecbd333eb11e27ac9b98d81075d5
Olegna753 <asalemme@metrostaff.it>
2025-06-19 20:17:35 +0100

    Delete firebase.js

--- firebase.js
+++
@@ -1,3 +0,0 @@
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"></script>
