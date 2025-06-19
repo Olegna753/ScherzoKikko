208c0b5df8bac197562ab03468373825f81f8f23
Olegna753 <asalemme@metrostaff.it>
2025-06-19 19:29:24 +0200

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,3 @@
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js"></script>
